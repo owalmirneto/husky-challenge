7bada992ed46887a6e71a67e184b5d8102a3d876
owalmirneto <owalmirneto@gmail.com>
2022-02-14 12:56:34 -0300

    refactor: remove rendering json on destroy invoice action

--- spec/support/shared_contexts/when_decorate_invoice.rb
+++ spec/support/shared_contexts/when_decorate_invoice.rb
@@ -2,7 +2,9 @@
 
 shared_context 'when decorate invoice' do
   before do
-    InvoiceDecorator.include(ActionView::Helpers::NumberHelper)
-    Invoice.include(InvoiceDecorator)
+    InvoiceDecorator.include?(ActionView::Helpers::NumberHelper) ||
+      InvoiceDecorator.include(ActionView::Helpers::NumberHelper)
+
+    Invoice.include?(InvoiceDecorator) || Invoice.include(InvoiceDecorator)
   end
 end
